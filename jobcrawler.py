c39c463a9d9f9a0d05cd66a9de43bd06edabc7e2
heyuno <haihoang09@yahoo.com>
2017-10-12 12:57:13 +0700

    Refactor

--- jobcrawler.py
+++ jobcrawler.py
@@ -1,8 +1,9 @@
 import json
-from sys import maxsize
+from sys import exit, maxsize
 
 import requests
 from lxml import html
+
 
 class ResultItem():
     """
@@ -44,6 +45,7 @@ class ResultItem():
 
 HOST = 'https://www.indeed.com'
 
+
 def search_job_indeed(keyword, location=None, zipcode=None, distance=None, max_no_result=maxsize, output_filename='data.json'):
 
     visited_url = set({})
@@ -54,9 +56,15 @@ def search_job_indeed(keyword, location=None, zipcode=None, distance=None, max_n
     next_url = start_url
     result_count = 0
     while next_url and result_count < max_no_result:
-        page = requests.get(next_url)
+        try:
+            page = requests.get(next_url)
+        # At the moment, we just capture all exception, may be handle later
+        except Exception as ex:
+            break
+
         tree = html.fromstring(page.content)
-        rows = tree.xpath('//div[contains(@class, "row") and contains(@class, "result")]')
+        rows = tree.xpath(
+            '//div[contains(@class, "row") and contains(@class, "result")]')
 
         for row in rows:
             item = extract_data(row)
@@ -64,31 +72,37 @@ def search_job_indeed(keyword, location=None, zipcode=None, distance=None, max_n
             if result_count > max_no_result:
                 break
 
-            # Do with scraped item
-            json_obj = {
-                "title": item.title,
-                "company": item.company,
-                "job_location": item.location,
-                "description": item.description,
-                "desired_experience": item.desired_experience,
-                "link": item.link
-            }
+            # If have job post
+            if item.title:
+                # Do with scraped item
+                json_obj = {
+                    "title": item.title,
+                    "company": item.company,
+                    "job_location": item.location,
+                    "description": item.description,
+                    "desired_experience": item.desired_experience,
+                    "link": item.link
+                }
+                jobs.append(json_obj)
 
-            jobs.append(json_obj)
-        
-        next_url = tree.xpath('//a[span[span[@class="np" and contains(text(),"Next")]]]/@href')
+        next_url = tree.xpath(
+            '//a[span[span[@class="np" and contains(text(),"Next")]]]/@href')
+
+        # If next url is visited before then quit
         if next_url and next_url[0] not in visited_url:
-            next_url = next_url[0]
+            next_url = HOST + next_url[0]
             visited_url.add(next_url)
         else:
             next_url = None
 
+    # Save jobs to json file
     with open(output_filename, 'a+') as f:
         json.dump(jobs, f)
 
 
 def extract_data(row):
     title = row.xpath('.//h2[@class="jobtitle"]/a/text()')
+    # result from xpath function is a list, we take first element
     title = title[0] if title else ''
 
     link = row.xpath('.//h2[@class="jobtitle"]/a/@href')
@@ -98,28 +112,37 @@ def extract_data(row):
         './/span[@class="company"]/span[@itemprop="name"]/a/text()')
     if not company:
         company = row.xpath(
-                './/span[@class="company"]/span[@itemprop="name"]/text()')
+            './/span[@class="company"]/span[@itemprop="name"]/text()')
     company = company[0] if company else ''
-
+    # print(company)
     job_location = row.xpath(
-            './/span[@itemprop="jobLocation"]/span/span/text()')
+        './/span[@itemprop="jobLocation"]/span/span/text()')
     job_location = job_location[0] if job_location else ''
 
     description = row.xpath(
-            './/div/span[@class="summary" and @itemprop="description"]/text()')
+        './/div/span[@class="summary" and @itemprop="description"]/text()')
     description = description[0] if description else ''
 
     desired_experience = row.xpath(
-            './/div[@class="experience"]/span[@class="experienceList"]/text()')
+        './/div[@class="experience"]/span[@class="experienceList"]/text()')
     desired_experience = desired_experience[0] if desired_experience else ''
 
     return ResultItem(_title=title, _company=company, _location=job_location, _date=None, _link=link, _description=description, _desired_experience=desired_experience)
 
 
 if __name__ == '__main__':
-    # keyword = input("Please enter the keyword, example: python ") or 'python'
-    # location = input("Please enter the location, example 'San Diego' ")
-    # max_result = input("Leave empty if you want to scrap as much as posible: ")
-    # max_result = int(max_result) if max_result else maxsize
-    # search_job_indeed(keyword, location)
-    search_job_indeed('python', 'San Diego', max_no_result=1)
+    print("""
+        You CAN leave empty in any field below
+        Ctrl + Z -> Enter to quit
+    """)
+
+    try:
+        keyword = input("Please enter the keyword (default is Python)  ") or 'Python'
+        location = input("Please enter the location (empty means any where)  ")
+        max_result = input("Max result (empty means as much as posible)  ")
+        max_result = int(max_result) if max_result else maxsize
+        output = input("Output file (default: data.json)  ") or 'data.json'
+        search_job_indeed(keyword, location, max_no_result=max_result)
+    # Ctrl+Z causes EOFError
+    except EOFError as identifier:
+        exit(1)
